756755016a7b995a01b1f9c8e467f1383f77516c
Karol Polak <karolpolak987@gmail.com>
2023-03-07 15:54:30 +0100

    Add material ui

---
+++ src/routes/Home/Home.jsx
@@ -0,0 +1,10 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+const Home = () => (
+  <div>
+    <Button variant="contained">Hello World</Button>
+  </div>
+)
+
+export default Home
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import ErrorPage from './Error/ErrorPage'
-import Root from './Root'
+import Home from './Home/Home'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Root />,
+    element: <Home />,
     errorElement: <ErrorPage />,
   },
 ])
