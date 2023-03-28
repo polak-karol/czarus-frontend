4510e873e81766ec29ae1eed483eb4f16af04cc9
polak-karol <karolpolak987@gmail.com>
2023-03-28 13:07:45 +0200

    Fix bug with changing month in calendar

--- src/routes/Holidays/index.jsx
+++ src/routes/Holidays/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import moment from 'moment'
-import { Grid, Paper, Stack, Typography } from '@mui/material'
+import { Alert, AlertTitle, Button, Grid, Stack, Typography } from '@mui/material'
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar'
 import { DayCalendarSkeleton } from '@mui/x-date-pickers/DayCalendarSkeleton'
 import agent from '~/api/agent'
@@ -10,7 +10,6 @@ import HolidayCard from './HolidayCard'
 
 const Holidays = () => {
   const [loading, setLoading] = useState(false)
-  const [loadingPage, setLoadingPage] = useState(true)
   const [holidaysData, setHolidaysData] = useState([1, 2, 15])
   const [selectedDate, setSelectedDate] = useState(moment())
   const [message, setMessage] = useState('')
@@ -21,16 +20,12 @@ const Holidays = () => {
   const getHolidaysSuccess = (response) => setHolidaysData(response.data)
 
   const getHolidays = (date) => {
-    setLoadingPage(true)
     const startOfMonth = date.startOf('month').format('YYYY-MM-DD')
     const endOfMonth = date.endOf('month').format('YYYY-MM-DD')
 
     return agent.Holidays.getHolidays('guild_id', { endDate: endOfMonth, startDate: startOfMonth })
       .then(getHolidaysSuccess, getHolidaysError)
-      .finally(() => {
-        setLoading(false)
-        setLoadingPage(false)
-      })
+      .finally(() => setLoading(false))
   }
 
   useEffect(() => {
@@ -41,17 +36,26 @@ const Holidays = () => {
     setLoading(true)
     setHolidaysData([])
     getHolidays(date)
-    return date
   }
 
-  if (loadingPage) return
-
   return (
-    <Stack direction="column">
+    <Stack direction="column" gap="2rem">
       <Typography variant="h3" component="h3">
         Holidays
       </Typography>
-      <Grid container gap="3rem">
+      <Alert
+        severity="warning"
+        variant="outlined"
+        action={
+          <Button color="inherit" size="small">
+            ADD
+          </Button>
+        }
+      >
+        <AlertTitle>Warning</AlertTitle>
+        No holiday for tomorrow!
+      </Alert>
+      <Grid container gap="6rem">
         <Grid xs={3} item>
           <DateCalendar
             value={selectedDate}
