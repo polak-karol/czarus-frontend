8e1fdda68aee80ea35ac2daf036326e18a4bd97a
polak-karol <karolpolak987@gmail.com>
2023-03-30 13:53:53 +0200

    Refactoring

--- src/routes/DrawChallanges/index.jsx
+++ src/routes/DrawChallanges/index.jsx
@@ -1,34 +1,6 @@
 import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
-import { Paper, Tab, Tabs } from '@mui/material'
+import TopBar from './TopBar'
 
-const DrawChallanges = () => {
-  const { pathname } = useLocation()
-
-  return (
-    <Paper>
-      <Tabs value={pathname}>
-        <Tab
-          component={Link}
-          value="/draw-challanges/writing"
-          to="/draw-challanges/writing"
-          label="Writing"
-        />
-        <Tab
-          component={Link}
-          value="/draw-challanges/painting"
-          to="/draw-challanges/painting"
-          label="Painting"
-        />
-        <Tab
-          component={Link}
-          value="/draw-challanges/music"
-          to="/draw-challanges/music"
-          label="Music"
-        />
-      </Tabs>
-    </Paper>
-  )
-}
+const DrawChallanges = () => <TopBar />
 
 export default DrawChallanges
