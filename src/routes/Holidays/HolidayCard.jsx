3b1bd3a66e50104a3ea875e4a343c84f06d7de76
polak-karol <karolpolak987@gmail.com>
2023-03-30 09:40:30 +0200

    Add alert when no holiday tomorrow

--- src/routes/Holidays/HolidayCard.jsx
+++ src/routes/Holidays/HolidayCard.jsx
@@ -1,27 +1,25 @@
 import React from 'react'
-import { Card, CardContent, CardHeader, Stack } from '@mui/material'
 import moment from 'moment'
+import { Card, CardContent, CardHeader, Stack } from '@mui/material'
 import { HolidayMessage, HolidayTimestamp } from './styles'
 
-const HolidayCard = ({ holiday }) => {
-  if (!holiday) return
-
-  return (
-    <Card>
-      <CardHeader title="Today" subheader={moment().format('DD-MM-YYYY')} />
-      <CardContent>
-        <HolidayMessage>{holiday.message}</HolidayMessage>
-        <Stack alignItems="flex-end">
-          <HolidayTimestamp>
-            {`Created at ${moment(holiday.createdAt).format('DD-MM-YYYY hh:mm:ss')}`}
-          </HolidayTimestamp>
-          <HolidayTimestamp>
-            {`Updated at ${moment(holiday.updatedAt).format('DD-MM-YYYY hh:mm:ss')}`}
-          </HolidayTimestamp>
-        </Stack>
-      </CardContent>
-    </Card>
-  )
-}
+const HolidayCard = ({ holiday }) => (
+  <Card>
+    <CardHeader title="Today" subheader={moment().format('DD-MM-YYYY')} />
+    <CardContent>
+      <HolidayMessage>{holiday?.message || 'No holiday for today provided.'}</HolidayMessage>
+      <Stack alignItems="flex-end">
+        <HolidayTimestamp>
+          {!!holiday?.createdAt &&
+            `Created at ${moment(holiday.createdAt).format('DD-MM-YYYY hh:mm:ss')}`}
+        </HolidayTimestamp>
+        <HolidayTimestamp>
+          {!!holiday?.createdAt &&
+            `Updated at ${moment(holiday.updatedAt).format('DD-MM-YYYY hh:mm:ss')}`}
+        </HolidayTimestamp>
+      </Stack>
+    </CardContent>
+  </Card>
+)
 
 export default HolidayCard
