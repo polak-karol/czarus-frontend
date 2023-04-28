311a5c452098fd45fc0f927f6b3cd61ef8d6fbed
karol-polak <karolpolak987@gmail.com>
2023-04-28 10:44:39 +0200

    Add profile menu

--- src/components/App/TopBar.jsx
+++ src/components/App/TopBar.jsx
@@ -1,18 +1,36 @@
-import React, { useContext } from 'react'
-import { Avatar, Box, Container, IconButton, Stack, Toolbar, Typography } from '@mui/material'
-import { Menu } from '@mui/icons-material'
+import React, { useContext, useState } from 'react'
+import {
+  Avatar,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Stack,
+  Toolbar,
+  Typography,
+} from '@mui/material'
+import { Menu as MenuIcon } from '@mui/icons-material'
 import UserContext from '~/contexts/UserContext'
 import { AppBar, isTopBarHidden } from './utils'
 
 const TopBar = ({ open, setOpen }) => {
+  const [anchorElUser, setAnchorElUser] = useState(null)
   const { user } = useContext(UserContext)
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget)
+  }
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null)
+  }
 
   if (isTopBarHidden()) return null
 
   return (
     <AppBar position="fixed" open={open}>
       <Container maxWidth="xl">
-        <Toolbar disableGutters>
+        <Toolbar>
           <IconButton
             color="inherit"
             aria-label="open drawer"
@@ -23,24 +41,58 @@ const TopBar = ({ open, setOpen }) => {
               ...(open && { display: 'none' }),
             }}
           >
-            <Menu />
+            <MenuIcon />
           </IconButton>
-          <Box sx={{ flexGrow: 1 }}>
+          <Stack
+            style={{ width: '100%' }}
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-between"
+          >
             <Typography variant="h6" noWrap component="div">
               Czaruś
             </Typography>
-          </Box>
-          <Box sx={{ flexGrow: 0 }}>
-            <Stack flexDirection="row" alignItems="center" gap=".5rem">
-              <Typography>{user.username}</Typography>
-              {user.avatar && (
-                <Avatar
-                  alt={user.username}
-                  src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}`}
-                />
-              )}
-            </Stack>
-          </Box>
+            {user.avatar && (
+              <>
+                <IconButton
+                  color="inherit"
+                  onClick={handleOpenUserMenu}
+                  edge="start"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                >
+                  <Avatar
+                    alt={user.username}
+                    src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}`}
+                  />
+                </IconButton>
+                <Menu
+                  sx={{ mt: '45px' }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={!!anchorElUser}
+                  onClose={handleCloseUserMenu}
+                >
+                  <MenuItem onClick={() => {}}>
+                    <Typography textAlign="left">Profile</Typography>
+                  </MenuItem>
+                  <MenuItem onClick={() => {}}>
+                    <Typography textAlign="left">Logout</Typography>
+                  </MenuItem>
+                </Menu>
+              </>
+            )}
+          </Stack>
         </Toolbar>
       </Container>
     </AppBar>
