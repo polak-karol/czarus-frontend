e84c107742d90f47ca5a14166b1daec67f8acf3d
karol-polak <karolpolak987@gmail.com>
2023-07-01 21:34:56 +0200

    Add select channel button on bithdays page

--- src/routes/Birthdays/index.jsx
+++ src/routes/Birthdays/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Button } from '@mui/material'
 import Page from '~/components/Page'
 import BirthdaysTable from './BirthdayTable'
 
 const Birthdays = () => (
-  <Page title="Birthdays">
+  <Page title="Birthdays" actions={<Button>Select channel</Button>}>
     <BirthdaysTable />
   </Page>
 )
