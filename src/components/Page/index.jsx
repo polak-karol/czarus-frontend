afed38dbf4367c490cb1b52133aaba2d4e20207d
karol-polak <karolpolak987@gmail.com>
2023-07-01 21:31:17 +0200

    Add actions to page component

--- src/components/Page/index.jsx
+++ src/components/Page/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Container, Stack, Typography } from '@mui/material'
 
-const Page = ({ title, children }) => (
+const Page = ({ title, children, actions }) => (
   <Container fixed>
     <Stack spacing={4}>
-      <Typography variant="h3" component="h3">
-        {title}
-      </Typography>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Typography variant="h3" component="h3">
+          {title}
+        </Typography>
+        {actions}
+      </Stack>
       {children}
     </Stack>
   </Container>
