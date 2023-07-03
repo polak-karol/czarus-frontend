074cedecd3bc88ad349c1a17d52f052b48190668
karol-polak <karolpolak987@gmail.com>
2023-07-03 19:36:46 +0200

    Add ChannelSelector on Birthdays page

--- src/routes/Birthdays/index.jsx
+++ src/routes/Birthdays/index.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import { Button } from '@mui/material'
+import React, { useState } from 'react'
 import Page from '~/components/Page'
 import BirthdaysTable from './BirthdayTable'
+import ChannelSelector from '~/components/ChannelSelector'
 
-const Birthdays = () => (
-  <Page title="Birthdays" actions={<Button>Select channel</Button>}>
-    <BirthdaysTable />
-  </Page>
-)
+const Birthdays = () => {
+  const [selectedChannel, setSelectedChannel] = useState('')
+
+  return (
+    <Page
+      title="Birthdays"
+      actions={
+        <ChannelSelector
+          selectedChannel={selectedChannel}
+          setSelectedChannel={(event) => setSelectedChannel(event.target.value)}
+        />
+      }
+    >
+      <BirthdaysTable />
+    </Page>
+  )
+}
 
 export default Birthdays
