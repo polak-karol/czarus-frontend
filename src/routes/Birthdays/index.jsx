6450177b7b922ce3864a8a8c129024b9429fb30b
karol-polak <karolpolak987@gmail.com>
2023-07-06 19:34:56 +0200

    Handle birthday channel id change

--- src/routes/Birthdays/index.jsx
+++ src/routes/Birthdays/index.jsx
@@ -1,18 +1,40 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import agent from '~/api/agent'
+import SelectedGuildContext from '~/contexts/SelectedGuildContext'
 import Page from '~/components/Page'
-import BirthdaysTable from './BirthdayTable'
 import ChannelSelector from '~/components/ChannelSelector'
+import BirthdaysTable from './BirthdayTable'
 
 const Birthdays = () => {
   const [selectedChannel, setSelectedChannel] = useState('')
+  const [loading, setLoading] = useState(false)
+  const { selectedGuild } = useContext(SelectedGuildContext)
+
+  const updateBirthdayChannelError = (error) => {
+    console.log(error)
+  }
+
+  const updateBirthdayChannelSuccess = (response) => {
+    setSelectedChannel(response.data.birthdays_channel_id)
+  }
+
+  const updateBirthdayChannel = (channel) => {
+    setLoading(true)
+    const body = { birthdays_channel_id: channel }
+
+    agent.GuildSettings.updateSettings(selectedGuild.id, body)
+      .then(updateBirthdayChannelSuccess, updateBirthdayChannelError)
+      .finally(() => setLoading(false))
+  }
 
   return (
     <Page
       title="Birthdays"
       actions={
         <ChannelSelector
+          disabled={loading}
           selectedChannel={selectedChannel}
-          setSelectedChannel={(event) => setSelectedChannel(event.target.value)}
+          setSelectedChannel={(event) => updateBirthdayChannel(event.target.value)}
         />
       }
     >
