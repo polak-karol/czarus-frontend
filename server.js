61508b25738d372688323ba3c16bb70ea52cb94b
polak-karol <karolpolak987@gmail.com>
2023-09-25 19:47:45 +0200

    Add express

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import path from 'path'
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, 'dist')))
+
+// Route all requests to the index.html for client-side routing
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
