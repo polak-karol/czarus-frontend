945ac9c73f03a2a769f77cd5fe551d3d7b507a86
polak-karol <karolpolak987@gmail.com>
2023-09-30 17:31:36 +0200

    Add birthdays settings

---
+++ src/routes/Settings/Subsetting/Birthdays/index.jsx
@@ -0,0 +1,88 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { Card, CardContent, Grid, Stack, Typography } from '@mui/material'
+import agent from '~/api/agent'
+import SelectedGuildContext from '~/contexts/SelectedGuildContext'
+import ChannelSelector from '~/components/ChannelSelector'
+
+const Birthdays = () => {
+  const { selectedGuild } = useContext(SelectedGuildContext)
+  const [selectedChannel, setSelectedChannel] = useState('')
+  const [loading, setLoading] = useState(true)
+
+  const updateBirthdaysChannelError = (error) => {
+    console.log(error)
+  }
+
+  const updateBirthdaysChannelSuccess = (response) => {
+    setSelectedChannel(response.data.birthdaysChannelId)
+  }
+
+  const updateBirthdaysChannel = (channel) => {
+    setLoading(true)
+    const body = { birthdaysChannelId: channel }
+
+    agent.GuildSettings.updateSettings(selectedGuild.id, body)
+      .then(updateBirthdaysChannelSuccess, updateBirthdaysChannelError)
+      .finally(() => setLoading(false))
+  }
+
+  const getGuildSettingsError = (error) => {
+    console.log(error)
+  }
+
+  const getGuildSettingsSuccess = (response) => {
+    console.log(response)
+    setSelectedChannel(response.data.birthdaysChannelId)
+  }
+
+  const getGuildSettings = () => {
+    setLoading(true)
+
+    return agent.GuildSettings.getSettings(selectedGuild.id)
+      .then(getGuildSettingsSuccess, getGuildSettingsError)
+      .then(() => setLoading(false))
+  }
+
+  useEffect(() => {
+    getGuildSettings()
+  }, [])
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={5}>
+        <Stack>
+          <Typography component="h6" variant="h6">
+            Basic settings
+          </Typography>
+          <Typography component="p" variant="p">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.{' '}
+          </Typography>
+        </Stack>
+      </Grid>
+      <Grid item xs={7}>
+        <Card>
+          <CardContent>
+            <Stack>
+              <ChannelSelector
+                fullWidth
+                disabled={loading}
+                selectedChannel={selectedChannel}
+                setSelectedChannel={(event) => updateBirthdaysChannel(event.target.value)}
+                helperText="Ipsam facere beatae nam tempore voluptas illum facilis."
+              />
+              <ChannelSelector
+                fullWidth
+                disabled={loading}
+                selectedChannel={selectedChannel}
+                setSelectedChannel={(event) => updateBirthdaysChannel(event.target.value)}
+                helperText="Ipsam facere beatae nam tempore voluptas illum facilis."
+              />
+            </Stack>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Birthdays
