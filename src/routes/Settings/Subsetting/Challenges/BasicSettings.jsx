39c9adaed623480a22c628ec947f77467c1f9d45
polak-karol <karolpolak987@gmail.com>
2023-10-05 21:14:06 +0200

    Refactoring

---
+++ src/routes/Settings/Subsetting/Challenges/BasicSettings.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { Formik } from 'formik'
+import { Button, Card, CardActions, CardContent, Grid, Stack, Typography } from '@mui/material'
+import ChannelSelector from '~/components/ChannelSelector'
+
+const BasicSettings = ({ challengesSettings, updateChallangesChannel }) => (
+  <>
+    <Grid item xs={5}>
+      <Stack>
+        <Typography component="h6" variant="h6">
+          Basic settings
+        </Typography>
+        <Typography component="p" variant="p">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        </Typography>
+      </Stack>
+    </Grid>
+    <Grid item xs={7}>
+      <Card>
+        <Formik
+          initialValues={{
+            drawChallengesWritingHandleChannelId:
+              challengesSettings.drawChallengesWritingHandleChannelId,
+            drawChallengesGraphicHandleChannelId:
+              challengesSettings.drawChallengesGraphicHandleChannelId,
+            drawChallengesMusicHandleChannelId:
+              challengesSettings.drawChallengesMusicHandleChannelId,
+          }}
+          onSubmit={updateChallangesChannel}
+        >
+          {({ values, setFieldValue, handleSubmit }) => (
+            <>
+              <CardContent>
+                <Stack>
+                  <ChannelSelector
+                    fullWidth
+                    selectedChannel={values.drawChallengesWritingHandleChannelId}
+                    setSelectedChannel={(event) =>
+                      setFieldValue('drawChallengesWritingHandleChannelId', event.target.value)
+                    }
+                    helperText="Writing challenges."
+                  />
+                  <ChannelSelector
+                    fullWidth
+                    selectedChannel={values.drawChallengesGraphicHandleChannelId}
+                    setSelectedChannel={(event) =>
+                      setFieldValue('drawChallengesGraphicHandleChannelId', event.target.value)
+                    }
+                    helperText="Graphic challenges"
+                  />
+                  <ChannelSelector
+                    fullWidth
+                    selectedChannel={values.drawChallengesMusicHandleChannelId}
+                    setSelectedChannel={(event) =>
+                      setFieldValue('drawChallengesMusicHandleChannelId', event.target.value)
+                    }
+                    helperText="Music challenges"
+                  />
+                </Stack>
+              </CardContent>
+              <CardActions>
+                <Button size="small" onClick={handleSubmit}>
+                  Save
+                </Button>
+              </CardActions>
+            </>
+          )}
+        </Formik>
+      </Card>
+    </Grid>
+  </>
+)
+
+export default BasicSettings
--- src/routes/Settings/Subsetting/Challenges/index.jsx
+++ src/routes/Settings/Subsetting/Challenges/index.jsx
@@ -1,10 +1,9 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { Formik } from 'formik'
-import { Button, Card, CardActions, CardContent, Grid, Stack, Typography } from '@mui/material'
+import { Grid } from '@mui/material'
 import agent from '~/api/agent'
 import SelectedGuildContext from '~/contexts/SelectedGuildContext'
-import ChannelSelector from '~/components/ChannelSelector'
 import PageSpinner from '~/components/PageSpinner'
+import BasicSettings from './BasicSettings'
 
 const Challenges = () => {
   const { selectedGuild } = useContext(SelectedGuildContext)
@@ -51,72 +50,10 @@ const Challenges = () => {
 
   return (
     <Grid container spacing={2}>
-      <Grid item xs={5}>
-        <Stack>
-          <Typography component="h6" variant="h6">
-            Basic settings
-          </Typography>
-          <Typography component="p" variant="p">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          </Typography>
-        </Stack>
-      </Grid>
-      <Grid item xs={7}>
-        <Card>
-          <Formik
-            initialValues={{
-              drawChallengesWritingHandleChannelId:
-                challengesSettings.drawChallengesWritingHandleChannelId,
-              drawChallengesGraphicHandleChannelId:
-                challengesSettings.drawChallengesGraphicHandleChannelId,
-              drawChallengesMusicHandleChannelId:
-                challengesSettings.drawChallengesMusicHandleChannelId,
-            }}
-            onSubmit={updateChallangesChannel}
-          >
-            {({ values, setFieldValue, handleSubmit }) => (
-              <>
-                <CardContent>
-                  <Stack>
-                    <ChannelSelector
-                      fullWidth
-                      disabled={loading}
-                      selectedChannel={values.drawChallengesWritingHandleChannelId}
-                      setSelectedChannel={(event) =>
-                        setFieldValue('drawChallengesWritingHandleChannelId', event.target.value)
-                      }
-                      helperText="Writing challenges."
-                    />
-                    <ChannelSelector
-                      fullWidth
-                      disabled={loading}
-                      selectedChannel={values.drawChallengesGraphicHandleChannelId}
-                      setSelectedChannel={(event) =>
-                        setFieldValue('drawChallengesGraphicHandleChannelId', event.target.value)
-                      }
-                      helperText="Graphic challenges"
-                    />
-                    <ChannelSelector
-                      fullWidth
-                      disabled={loading}
-                      selectedChannel={values.drawChallengesMusicHandleChannelId}
-                      setSelectedChannel={(event) =>
-                        setFieldValue('drawChallengesMusicHandleChannelId', event.target.value)
-                      }
-                      helperText="Music challenges"
-                    />
-                  </Stack>
-                </CardContent>
-                <CardActions>
-                  <Button size="small" onClick={handleSubmit}>
-                    Save
-                  </Button>
-                </CardActions>
-              </>
-            )}
-          </Formik>
-        </Card>
-      </Grid>
+      <BasicSettings
+        challengesSettings={challengesSettings}
+        updateChallangesChannel={updateChallangesChannel}
+      />
     </Grid>
   )
 }
