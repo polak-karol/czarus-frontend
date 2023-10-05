24e7830deecb3647e5f7a9c26574acc8583dc4f1
polak-karol <karolpolak987@gmail.com>
2023-10-05 21:32:14 +0200

    Refactoring

---
+++ src/routes/Settings/Subsetting/Holidays/BasicSettings.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Formik } from 'formik'
+import { Button, Card, CardActions, CardContent, Grid, Stack, Typography } from '@mui/material'
+import ChannelSelector from '~/components/ChannelSelector'
+
+const BasicSettings = ({ holidaysSettings, updateHolidaysChannel }) => (
+  <>
+    <Grid item xs={5}>
+      <Stack>
+        <Typography component="h2" variant="h2">
+          Basic settings
+        </Typography>
+        <Typography component="p" variant="p">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        </Typography>
+      </Stack>
+    </Grid>
+    <Grid item xs={7}>
+      <Card>
+        <Formik
+          initialValues={{
+            holidayAnnouncementChannelId: holidaysSettings.holidayAnnouncementChannelId,
+          }}
+          onSubmit={updateHolidaysChannel}
+        >
+          {({ values, setFieldValue, handleSubmit }) => (
+            <>
+              <CardContent>
+                <ChannelSelector
+                  fullWidth
+                  selectedChannel={values.holidayAnnouncementChannelId}
+                  setSelectedChannel={(event) =>
+                    setFieldValue('holidayAnnouncementChannelId', event.target.value)
+                  }
+                  helperText="Ipsam facere beatae nam tempore voluptas illum facilis."
+                />
+              </CardContent>
+              <CardActions>
+                <Button size="small" onClick={handleSubmit}>
+                  Save
+                </Button>
+              </CardActions>
+            </>
+          )}
+        </Formik>
+      </Card>
+    </Grid>
+  </>
+)
+
+export default BasicSettings
