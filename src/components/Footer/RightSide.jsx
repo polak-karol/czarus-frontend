cd6819490315e7acbbde67ee4a185276405f9b4b
polak-karol <karolpolak987@gmail.com>
2023-10-07 19:29:50 +0200

    Refactor RightSide in Footer component

--- src/components/Footer/RightSide.jsx
+++ src/components/Footer/RightSide.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Typography } from '@mui/material'
 
 const RightSide = () => (
-  <Typography sx={{ padding: '0rem 0' }} component="p" variant="p">
+  <Typography sx={{ padding: '0rem 0' }} component="span" variant="span">
     © 2023 Czaruś - Discord Bot
   </Typography>
 )
